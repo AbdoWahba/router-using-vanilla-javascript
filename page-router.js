7485a010e336f19e4ebc56b2b37e84d071977287
AbdoWahba <abdo.tw20154@gmail.com>
2019-08-08 21:55:47 -0700

    adding function that check for the elements(scripts and styles) to add it to head tag if it not exist then make a call back for this function in callPage function

--- page-router.js
+++ page-router.js
@@ -1,4 +1,5 @@
-let callPage = form_path => {
+let callPage = (form_path, addfn) => {
+  debugger;
   let xhr = new XMLHttpRequest();
 
   xhr.responseType = "document";
@@ -8,11 +9,72 @@ let callPage = form_path => {
   xhr.onload = () => {
     if (xhr.status == 200) {
       body = new XMLSerializer().serializeToString(xhr.response.body);
-      documentJson = xml2json(this.response, "");
-
-      return { body: body, documentJson: documentJson };
+      headXML = xhr.response.head;
+      var ret = "";
+      ret = { body: body, headXML: headXML };
+      addfn(headXML);
+      console.log(ret);
     }
   };
 
   xhr.send();
 };
+
+appendHead = headXML => {
+  console.log(headXML);
+  var newElements = headXML.getElementsByTagName("script");
+  var oldElements = document.getElementsByTagName("script");
+  var can_be_appended = true;
+  console.log("eeh ba2a1");
+  for (let newElement = 0; newElement < newElements.length; newElement++) {
+    can_be_appended = true;
+    for (let oldElement = 0; oldElement < oldElements.length; oldElement++) {
+      console.log(newElements[newElement].src);
+      console.log(oldElements[oldElement].src);
+      if (newElements[newElement].src === oldElements[oldElement].src) {
+        can_be_appended = false;
+        console.log(newElements[newElement].src);
+        console.log(oldElements[oldElement].src);
+        break;
+      }
+      console.log("eeh ba2a3");
+    }
+    console.log("eeh ba2a4");
+    if (can_be_appended) {
+      head = document.getElementsByTagName("head")[0];
+      console.log(newElement);
+      console.log(newElements, newElement);
+      head.appendChild(newElements[newElement]);
+      console.log("eeh ba2a5");
+    }
+  }
+
+  var newElements = headXML.getElementsByTagName("link");
+  var oldElements = document.getElementsByTagName("link");
+  var can_be_appended = true;
+  console.log("eeh ba2a1");
+  for (let newElement = 0; newElement < newElements.length; newElement++) {
+    can_be_appended = true;
+    for (let oldElement = 0; oldElement < oldElements.length; oldElement++) {
+      console.log(newElements[newElement].href);
+      console.log(oldElements[oldElement].href);
+      if (newElements[newElement].href === oldElements[oldElement].href) {
+        can_be_appended = false;
+        console.log(newElements[newElement].href);
+        console.log(oldElements[oldElement].href);
+        break;
+      }
+      console.log("eeh ba2a3");
+    }
+    console.log("eeh ba2a4");
+    if (can_be_appended) {
+      head = document.getElementsByTagName("head")[0];
+      console.log(newElement);
+      console.log(newElements, newElement);
+      head.appendChild(newElements[newElement]);
+      console.log("eeh ba2a5");
+    }
+  }
+};
+debugger;
+callPage("./dataforms/bodyform.html", appendHead);
